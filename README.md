50df38733756590df1598d72b2ea3b18d84eca9e
Arthur Ngondo <ngondoarthur@gmail.com>
2017-06-13 13:31:29 +0300

    Car Class Lab

---
+++ README.md
@@ -0,0 +1,5 @@
+# Andela Bootcamp Lab - Car Class
+A car class that can be used to instantiate various vehicles.
+
+It takes in arguments that depict the type, model, and name of the vehicle, provided they are set.
+
---
+++ car_class.py
@@ -0,0 +1,31 @@
+class Car(object):
+       
+  def __init__(self, name="General", model="GM", car_type="saloon", speed=0):
+    self.name = name
+    self.model = model
+    self.car_type = car_type
+    self.speed = speed
+    self.num_of_wheels=4
+    
+    if self.name == "Porshe" or self.name == "Koenigsegg":
+      self.num_of_doors = 2
+    else:
+      self.num_of_doors = 4
+    
+    if self.car_type=="trailer":
+      self.num_of_wheels = 8
+      self.speed = 0    
+      
+  def is_saloon(self):
+    if self.car_type is not "trailer":
+      # self.type = "saloon"
+      return True
+    return False
+      
+  def drive(self, speed):
+    if self.car_type is not "trailer":
+      self.speed = 10**speed
+    else:
+      self.speed = 77
+    return self
+
